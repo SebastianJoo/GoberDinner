75e24910b5de05917a02e3abf034b23711c2651b
Sebastian Johansson <sebastian.johansson@hm.com>
2022-06-19 21:23:06 +0200

    add token generation

--- GoberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ GoberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -1,17 +1,32 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using BuberDinner.Application.Common.Interfaces.Authentication;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GoberDinner.Infrastructure.Authentication;
 public class JwtTokenGenerator : IJwtTokenGenerator
 {
     public string GenerateToken(Guid userId, string firstName, string lastName)
     {
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes("super-secret-key")),
+            SecurityAlgorithms.HmacSha256);
+
         var claims = new[]
         {
-            new Claim(JwtRegisteredClaimNames.Sub, firstName + " " + lastName),
+            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim(JwtRegisteredClaimNames.UniqueName, userId.ToString())
         };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: "GoberDinner",
+            expires: DateTime.Now.AddMinutes(60),
+            claims: claims,
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
     }
 }
