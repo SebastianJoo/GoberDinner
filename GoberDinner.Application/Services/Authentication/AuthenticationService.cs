d078d199b0c81fac2935c1b61c8c5a2acfa308ca
Sebastian Johansson <seb.johansson@outlook.com>
2022-06-20 20:52:33 +0200

    add first implementation of Register and Login

--- GoberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ GoberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,29 +1,43 @@
 using BuberDinner.Application.Common.Interfaces.Authentication;
+using GoberDinner.Application.Common.Interfaces.Persistance;
+using GoberDinner.Domain.Entities;
 
 namespace GoberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
 
-    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
     {
         _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        // Check if user already exists
+        //1. Validate the user email already exists
+        if (_userRepository.GetUserByEmail(email) is not null)
+        {
+            throw new Exception("User with given email already exists");
+        }
 
-        // If not, create user (generate unique ID)
+        //2. Create the user
+        var user = new User
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            Password = password
+        };
+        //3. Add the user to the database
+        _userRepository.Add(user);
 
-        // Generate JWT token
-        Guid userId = Guid.NewGuid();
-
-        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+        var token = _jwtTokenGenerator.GenerateToken(user.Id, firstName, lastName);
 
         return new AuthenticationResult(
-            userId,
+            user.Id,
             firstName,
             lastName,
             email,
@@ -32,11 +46,24 @@ public class AuthenticationService : IAuthenticationService
 
     public AuthenticationResult Login(string email, string password)
     {
+        //1. Validate that the user exists
+        if (_userRepository.GetUserByEmail(email) is not User user)
+        {
+            throw new Exception("User with given email does not exist");
+        }
+        //2. Validate that the password is correct
+        if (user.Password != password)
+        {
+            throw new Exception("Invalid password");
+        }
+        //3. Generate the token
+        var token = _jwtTokenGenerator.GenerateToken(user.Id, user.FirstName, user.LastName);
+
         return new AuthenticationResult(
-            Guid.NewGuid(),
-            "firstName",
-            "lastName",
+            user.Id,
+            user.FirstName,
+            user.LastName,
             email,
-            "token");
+            token);
     }
 }
